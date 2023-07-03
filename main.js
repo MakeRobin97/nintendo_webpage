d86e1e5c35092c30a6a958312bfa14138fc84f0c
MakeRobin97 <pkook7@gmail.com>
2023-07-03 20:23:51 +0900

    230703

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 사이즈 조절
+
 window.addEventListener('resize',function(){
   console.log(window.innerWidth)
   if(window.innerWidth<=1230){
@@ -6,11 +8,17 @@ window.addEventListener('resize',function(){
     document.querySelectorAll('.bottom_inf_list_content')[2].classList.add('hide')
     document.querySelectorAll('.bottom_inf_list_content')[3].classList.add('hide')
 
+    document.querySelector('.exit_btn').classList.remove('hide')
+
+
   } else {
     document.querySelectorAll('.bottom_inf_list_content')[0].classList.remove('hide')
     document.querySelectorAll('.bottom_inf_list_content')[1].classList.remove('hide')
     document.querySelectorAll('.bottom_inf_list_content')[2].classList.remove('hide')
     document.querySelectorAll('.bottom_inf_list_content')[3].classList.remove('hide')
+
+    document.querySelector('.exit_btn').classList.add('hide')
+
   }
 })
 
@@ -22,8 +30,15 @@ window.addEventListener('load',function(){
     document.querySelectorAll('.bottom_inf_list_content')[2].classList.add('hide')
     document.querySelectorAll('.bottom_inf_list_content')[3].classList.add('hide')
 
+    document.querySelector('.exit_btn').classList.remove('hide')
+
+} else {
+    document.querySelector('.exit_btn').classList.add('hide')
 }
 })
+
+
+
 
 
 
@@ -46,3 +61,34 @@ document.querySelectorAll('.bottom_inf_list_title')[3].addEventListener('click',
   if(window.innerWidth<=1230){
   document.querySelectorAll('.bottom_inf_list_content')[3].classList.toggle('hide');}
 })
+
+
+// 네비바
+document.querySelector('.navbar_device').addEventListener('mouseover',function(){
+
+  document.querySelector('.navbar_device').style.color='red';
+  document.querySelector('.navbar_device i').style.color='red';
+  document.querySelector('.navbar_device').style.borderBottom = "4px solid red";
+
+  if(window.innerWidth>=1230){
+  document.querySelector('.navbar_device_inf').style.height='280px'
+  } else if ((window.innerWidth<1230 && window.innerWidth>868)){
+    document.querySelector('.navbar_device_inf').style.height='290px'
+  } else if((window.innerWidth<=868)) {
+    document.querySelector('.navbar_device_inf').style.height='500px'
+  }
+})
+
+document.querySelector('.navbar_device').addEventListener('mouseout',function(){
+  document.querySelector('.navbar_device').style.color='black';
+  document.querySelector('.navbar_device i').style.color='#C0C0C0';
+  document.querySelector('.navbar_device').style.borderBottom = "1px solid silver";
+
+
+  document.querySelector('.navbar_device_inf').style.height='0px'
+
+})
+
+
+
+
